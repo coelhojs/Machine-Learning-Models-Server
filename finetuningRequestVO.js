459701f1e6549a6cbc107663b9f9a216cf260541
Your Name <Cristiano Coelho Souza>
2019-12-15 23:25:58 -0300

    preparando fine-tuner

---
+++ finetuningRequestVO.js
@@ -0,0 +1,10 @@
+request = {
+    request_id: 1,
+    model_name: "Vera-Species",
+    current_version: "1",
+    current_classes: ["coqueiro", "mangueira", "oiti", "sibipiruna"],
+    new_classes: ["limoeiro"],
+    dataset_path: "",
+    num_steps: "",
+    
+}
---
+++ tensorflow_scripts/image_classification/finetuning_classifier.py
@@ -0,0 +1,331 @@
+# USAGE
+# python train.py
+
+import collections
+import hashlib
+import os.path
+import pickle
+import random
+import re
+import sys
+from datetime import datetime
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers.core import Dense, Dropout, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import tensorflow_hub as hub
+from imutils import paths
+from sklearn.metrics import classification_report
+
+matplotlib.use("Agg")
+
+config = {
+    # initialize the path to the *original* input directory of images
+    ORIG_INPUT_DATASET = "Food-11"
+
+    # initialize the base path to the *new* directory that will contain
+    # our images after computing the training and testing split
+    BASE_PATH = "dataset"
+
+    # define the names of the training, testing, and validation
+    # directories
+    TRAIN = "training"
+    TEST = "evaluation"
+    VAL = "validation"
+
+    # initialize the list of class label names
+    CLASSES = ["Bread", "Dairy product", "Dessert", "Egg", "Fried food",
+        "Meat", "Noodles/Pasta", "Rice", "Seafood", "Soup",
+        "Vegetable/Fruit"]
+
+    # set the batch size when fine-tuning
+    BATCH_SIZE = 32
+
+    # initialize the label encoder file path and the output directory to
+    # where the extracted features (in CSV file format) will be stored
+    LE_PATH = os.path.sep.join(["output", "le.cpickle"])
+    BASE_CSV_PATH = "output"
+
+    # set the path to the serialized model after training
+    MODEL_PATH = os.path.sep.join(["output", "food11.model"])
+
+    # define the path to the output training history plots
+    UNFROZEN_PLOT_PATH = os.path.sep.join(["output", "unfrozen.png"])
+    WARMUP_PLOT_PATH = os.path.sep.join(["output", "warmup.png"])
+}
+
+#TODO: Trocar pela quantidade de imagens da classe que possuir menos imagens
+MAX_NUM_IMAGES_PER_CLASS = 2 ** 27 - 1  # ~134M
+
+#TODO: Tornar este método um útil, que pega a pasta do parâmetro e organiza as 
+# imagens na estrutura train/validation/test se essa estrutura não estiver criada
+# e verifica se as imagens estão distribuídas na proporção desejada; se não estiverem,
+# move as imagens para atender aos parametros definidos.
+def create_image_lists(image_dir, testing_percentage, validation_percentage):
+  """Builds a list of training images from the file system.
+
+  Analyzes the sub folders in the image directory, splits them into stable
+  training, testing, and validation sets, and returns a data structure
+  describing the lists of images for each label and their paths.
+
+  Args:
+    image_dir: String path to a folder containing subfolders of images.
+    testing_percentage: Integer percentage of the images to reserve for tests.
+    validation_percentage: Integer percentage of images reserved for validation.
+
+  Returns:
+    An OrderedDict containing an entry for each label subfolder, with images
+    split into training, testing, and validation sets within each label.
+    The order of items defines the class indices.
+  """
+  if not tf.gfile.Exists(image_dir):
+    tf.logging.error("Image directory '" + image_dir + "' not found.")
+    return None
+  result = collections.OrderedDict()
+  sub_dirs = sorted(x[0] for x in tf.gfile.Walk(image_dir))
+  # The root directory comes first, so skip it.
+  is_root_dir = True
+  for sub_dir in sub_dirs:
+    if is_root_dir:
+      is_root_dir = False
+      continue
+    extensions = sorted(set(os.path.normcase(ext)  # Smash case on Windows.
+                            for ext in ['JPEG', 'JPG', 'jpeg', 'jpg', 'png']))
+    file_list = []
+    dir_name = os.path.basename(
+        # tf.gfile.Walk() returns sub-directory with trailing '/' when it is in
+        # Google Cloud Storage, which confuses os.path.basename().
+        sub_dir[:-1] if sub_dir.endswith('/') else sub_dir)
+
+    if dir_name == image_dir:
+      continue
+    tf.logging.info("Looking for images in '" + dir_name + "'")
+    for extension in extensions:
+      file_glob = os.path.join(image_dir, dir_name, '*.' + extension)
+      file_list.extend(tf.gfile.Glob(file_glob))
+    if not file_list:
+      tf.logging.warning('No files found')
+      continue
+    if len(file_list) < 20:
+      tf.logging.warning(
+          'WARNING: Folder has less than 20 images, which may cause issues.')
+    elif len(file_list) > MAX_NUM_IMAGES_PER_CLASS:
+      tf.logging.warning(
+          'WARNING: Folder {} has more than {} images. Some images will '
+          'never be selected.'.format(dir_name, MAX_NUM_IMAGES_PER_CLASS))
+    label_name = re.sub(r'[^a-z0-9]+', ' ', dir_name.lower())
+    training_images = []
+    testing_images = []
+    validation_images = []
+    for file_name in file_list:
+      base_name = os.path.basename(file_name)
+      # We want to ignore anything after '_nohash_' in the file name when
+      # deciding which set to put an image in, the data set creator has a way of
+      # grouping photos that are close variations of each other. For example
+      # this is used in the plant disease data set to group multiple pictures of
+      # the same leaf.
+      hash_name = re.sub(r'_nohash_.*$', '', file_name)
+      # This looks a bit magical, but we need to decide whether this file should
+      # go into the training, testing, or validation sets, and we want to keep
+      # existing files in the same set even if more files are subsequently
+      # added.
+      # To do that, we need a stable way of deciding based on just the file name
+      # itself, so we do a hash of that and then use that to generate a
+      # probability value that we use to assign it.
+      hash_name_hashed = hashlib.sha1(tf.compat.as_bytes(hash_name)).hexdigest()
+      percentage_hash = ((int(hash_name_hashed, 16) %
+                          (MAX_NUM_IMAGES_PER_CLASS + 1)) *
+                         (100.0 / MAX_NUM_IMAGES_PER_CLASS))
+      if percentage_hash < validation_percentage:
+        validation_images.append(base_name)
+      elif percentage_hash < (testing_percentage + validation_percentage):
+        testing_images.append(base_name)
+      else:
+        training_images.append(base_name)
+    result[label_name] = {
+        'dir': dir_name,
+        'training': training_images,
+        'testing': testing_images,
+        'validation': validation_images,
+    }
+  return result
+
+
+def plot_training(H, N, plotPath):
+	# construct a plot that plots and saves the training history
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+	plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+	plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+	plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="lower left")
+	plt.savefig(plotPath)
+
+# derive the paths to the training, validation, and testing
+# directories
+trainPath = os.path.sep.join([config.BASE_PATH, config.TRAIN])
+valPath = os.path.sep.join([config.BASE_PATH, config.VAL])
+testPath = os.path.sep.join([config.BASE_PATH, config.TEST])
+
+# determine the total number of image paths in training, validation,
+# and testing directories
+totalTrain = len(list(paths.list_images(trainPath)))
+totalVal = len(list(paths.list_images(valPath)))
+totalTest = len(list(paths.list_images(testPath)))
+
+# initialize the training data augmentation object
+trainAug = ImageDataGenerator(
+	rotation_range=30,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+# initialize the validation/testing data augmentation object (which
+# we'll be adding mean subtraction to)
+valAug = ImageDataGenerator()
+
+# define the ImageNet mean subtraction (in RGB order) and set the
+# the mean subtraction value for each of the data augmentation
+# objects
+mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+trainAug.mean = mean
+valAug.mean = mean
+
+# initialize the training generator
+trainGen = trainAug.flow_from_directory(
+	trainPath,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=True,
+	batch_size=config.BATCH_SIZE)
+
+# initialize the validation generator
+valGen = valAug.flow_from_directory(
+	valPath,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=config.BATCH_SIZE)
+
+# initialize the testing generator
+testGen = valAug.flow_from_directory(
+	testPath,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=config.BATCH_SIZE)
+
+# load the VGG16 network, ensuring the head FC layer sets are left
+# off
+baseModel = VGG16(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+# construct the head of the model that will be placed on top of the
+# the base model
+headModel = baseModel.output
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(512, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(len(config.CLASSES), activation="softmax")(headModel)
+
+# place the head FC model on top of the base model (this will become
+# the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# loop over all layers in the base model and freeze them so they will
+# *not* be updated during the first training process
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# compile our model (this needs to be done after our setting our
+# layers to being non-trainable
+print("[INFO] compiling model...")
+opt = SGD(lr=1e-4, momentum=0.9)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train the head of the network for a few epochs (all other layers
+# are frozen) -- this will allow the new FC layers to start to become
+# initialized with actual "learned" values versus pure random
+print("[INFO] training head...")
+H = model.fit_generator(
+	trainGen,
+	steps_per_epoch=totalTrain // config.BATCH_SIZE,
+	validation_data=valGen,
+	validation_steps=totalVal // config.BATCH_SIZE,
+	epochs=50)
+
+# reset the testing generator and evaluate the network after
+# fine-tuning just the network head
+print("[INFO] evaluating after fine-tuning network head...")
+testGen.reset()
+predIdxs = model.predict_generator(testGen,
+	steps=(totalTest // config.BATCH_SIZE) + 1)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testGen.classes, predIdxs,
+	target_names=testGen.class_indices.keys()))
+plot_training(H, 50, config.WARMUP_PLOT_PATH)
+
+# reset our data generators
+trainGen.reset()
+valGen.reset()
+
+# now that the head FC layers have been trained/initialized, lets
+# unfreeze the final set of CONV layers and make them trainable
+for layer in baseModel.layers[15:]:
+	layer.trainable = True
+
+# loop over the layers in the model and show which ones are trainable
+# or not
+for layer in baseModel.layers:
+	print("{}: {}".format(layer, layer.trainable))
+
+# for the changes to the model to take affect we need to recompile
+# the model, this time using SGD with a *very* small learning rate
+print("[INFO] re-compiling model...")
+opt = SGD(lr=1e-4, momentum=0.9)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train the model again, this time fine-tuning *both* the final set
+# of CONV layers along with our set of FC layers
+H = model.fit_generator(
+	trainGen,
+	steps_per_epoch=totalTrain // config.BATCH_SIZE,
+	validation_data=valGen,
+	validation_steps=totalVal // config.BATCH_SIZE,
+	epochs=20)
+
+# reset the testing generator and then use our trained model to
+# make predictions on the data
+print("[INFO] evaluating after fine-tuning network...")
+testGen.reset()
+predIdxs = model.predict_generator(testGen,
+	steps=(totalTest // config.BATCH_SIZE) + 1)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testGen.classes, predIdxs,
+	target_names=testGen.class_indices.keys()))
+plot_training(H, 20, config.UNFROZEN_PLOT_PATH)
+
+# serialize the model to disk
+print("[INFO] serializing network...")
+model.save(config.MODEL_PATH)
